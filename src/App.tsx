46b23a7029b8a282cf22a12149d5c1ea69ec96c9
Huam Benvenutti <huambenvenutti@protonmail.com>
2022-11-03 17:38:14 -0300

    eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
-import React from 'react';
+import React from 'react'
 
 import { ThemeProvider } from 'styled-components'
-import { GlobalStyle } from './styles/global';
+import { GlobalStyle } from './styles/global'
 import { defaultTheme } from './styles/themes/default'
 
 export function App() {
-
   return (
     <ThemeProvider theme={defaultTheme}>
-      <GlobalStyle/>
+      <GlobalStyle />
     </ThemeProvider>
   )
 }
--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,14 +1,25 @@
-import { createGlobalStyle } from "styled-components";
+import { createGlobalStyle } from 'styled-components'
 
 export const GlobalStyle = createGlobalStyle`
   * {
     margin: 0;
     padding: 0;
     box-sizing: border-box;
+    font-family: 'Roboto Mono', monospace;
+  }
+
+  :focus {
+    outline: 0;
+    box-shadow: 0 0 0 2px ${(props) => props.theme['green-500']};
   }
 
   body {
-    background: #333;
-    color: #fff;
+    background: ${(props) => props.theme['gray-900']};
+    color: ${(props) => props.theme['gray-300']};
+  }
+
+  body, input, textarea, button {
+    font-family: 'Roboto', sans-serif;
+    font-weight: 400;
   }
 `
