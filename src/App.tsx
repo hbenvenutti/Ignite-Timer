1709a7d1fb8c8842aefebbbd90983340f6a37325
Huam Benvenutti <huambenvenutti@protonmail.com>
2022-11-05 18:14:47 -0300

    header

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,7 @@
 import React from 'react'
 
-import { ThemeProvider } from 'styled-components'
 import { GlobalStyle } from './styles/global'
-import { defaultTheme } from './styles/themes/default'
 
 export function App() {
-  return (
-    <ThemeProvider theme={defaultTheme}>
-      <GlobalStyle />
-    </ThemeProvider>
-  )
+  return <GlobalStyle />
 }
--- src/styles/global.ts
+++ src/styles/global.ts
@@ -5,7 +5,6 @@ export const GlobalStyle = createGlobalStyle`
     margin: 0;
     padding: 0;
     box-sizing: border-box;
-    font-family: 'Roboto Mono', monospace;
   }
 
   :focus {
@@ -21,5 +20,6 @@ export const GlobalStyle = createGlobalStyle`
   body, input, textarea, button {
     font-family: 'Roboto', sans-serif;
     font-weight: 400;
+    font-size: 1rem;
   }
 `
